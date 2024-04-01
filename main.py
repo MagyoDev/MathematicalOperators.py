5cb01ca312de4fe08129b70bb0a1447daf17e138
MagyoDev <alandelimasilva51@gmail.com>
2024-04-01 12:50:09 -0300

    Mathematical Operators

---
+++ main.py
@@ -0,0 +1,15 @@
+# Mathematical Operators
+N1 = float(input("Number 1: "))
+N2 = float(input("Number 2: "))
+
+sum = N1 + N2
+sub = N1 - N2
+mul = N1 * N2
+div = N1 / N2
+div2 = N1 // N2
+pot = N1 ** N2
+rest = N1 % N2
+
+print("Sum is {:.2f}, Sub is {:.2f}, Mul is {:.2f}, Div is {:.2f}, Div2 {:.2f}, Pot is {:.2f}, Rest is {:.2f}"
+      .format(sum,sub,mul,div,div2,pot,rest))
+
